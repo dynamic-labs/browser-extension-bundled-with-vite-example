cb24d17435e04f7c21c82527d572aff8bfe42e1a
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2025-01-17 12:50:23 -0300

    remove apple cdn code

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import path from "path";
+
+const replaceLoadAppleIdScriptWithCustomScriptPlugin = {
+  name: "replace-loadAppleId",
+  enforce: "pre",
+  resolveId(source) {
+    if (source.includes("loadAppleId")) {
+      return path.resolve(__dirname, "src/loadAppleId.js");
+    }
+  },
+} as const;
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), replaceLoadAppleIdScriptWithCustomScriptPlugin],
   optimizeDeps: {
     esbuildOptions: {
       // Enable esbuild polyfill plugins
